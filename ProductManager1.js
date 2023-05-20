1d79afd8be1c1f224a63d7bf0f1b9005dcb64847
Constanza Repetto <repettocj@gmail.com>
2023-05-20 16:00:53 -0300

    cambiossss final

--- ProductManager1.js
+++ ProductManager1.js
@@ -12,34 +12,52 @@
 // Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
 // Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
 
-class ProductManager{
-    constructor(){
-        this.products = [];
-    }
 
-    
+
+class ProductManager {
+
+    #products
+
+    constructor() {
+        this.#products = [];
+    }
 
     addProduct = (title, description, price, thumbnail, code, stock) => {
         const product = {
-            id: this.products.length + 1,
-            title,   
+            id: this.#products.length + 1,
+            title,
             description,
             price,
             thumbnail,
             code,
             stock
         }
-        if (!this.products.includes(product.code)){
-            this.products.push(product);
+        const existsProduct = this.#products.find((element) => {
+            return element.id == product.id
+        })
+        const existsCode = this.#products.find((element) => {
+            return element.code == product.code
+        })
+
+
+        if (existsProduct || existsCode) {
+            console.log('Error: Producto ya agregado a la lista')
         } else {
-            console.log('Producto ya agregado a la lista')
+            this.#products.push(product)
+            console.log("Producto Nuevo agregado correctamente");
         }
-        
     }
 
     getProducts() {
-        return this.products;
+        return this.#products;
     }
+
+    getProductById(id) {
+        return this.#products.find((element) => {
+            return element.id == id
+        }) || 'ID no encontrado';
+    }
+
 }
 
 let product1 = new ProductManager()
@@ -47,5 +65,8 @@ let product1 = new ProductManager()
 
 
 console.log(product1.addProduct('queso', 'queso', 200, 'no def', 'abc1', 10))
-console.log(product1.addProduct('quesito', 'queso', 300, 'no def', 'abc1', 100))
+console.log(product1.addProduct('quesito', 'queso', 300, 'no def', 'abc2', 100))
+console.log(product1.addProduct('quesito', 'queso', 300, 'no def', 'abc2', 100))
 console.log(product1.getProducts())
+console.log(product1.getProductById(1));
+console.log(product1.getProductById(3));
